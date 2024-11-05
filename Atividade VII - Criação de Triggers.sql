d2f25d012db4afebf17e55e7eac540c2e8d4718e
Michael <158838095+InteligenteMaia@users.noreply.github.com>
2024-11-05 18:28:43 -0300

    Update Atividade VII - Criação de Triggers.sql

--- Atividade VII - Criação de Triggers.sql
+++ Atividade VII - Criação de Triggers.sql
@@ -9,7 +9,7 @@ CREATE TABLE log (
     valor_novo VARCHAR(500)
 );
 
--- 2) Configurar uma Sequência para o ID do Log 
+-- 2) Configurar uma SequÃªncia para o ID do Log 
 
 CREATE SEQUENCE log_seq START WITH 1 INCREMENT BY 1;
 
@@ -17,58 +17,51 @@ CREATE SEQUENCE log_seq START WITH 1 INCREMENT BY 1;
 -- a.	Trigger para INSERT na tabela Produto
 
 CREATE OR REPLACE TRIGGER log_insert_produto
-AFTER INSERT ON Produto
+AFTER INSERT ON Produtos
 FOR EACH ROW
 BEGIN
   INSERT INTO log (id, nome_tabela, operacao, valor_novo)
   VALUES (log_seq.NEXTVAL, 'Produto', 'INSERT', :NEW.nome);
 END;
 
----
-
-CREATE OR REPLACE TRIGGER log_insert_produto
-AFTER INSERT ON Produto
-FOR EACH ROW
-BEGIN
-  INSERT INTO log (id, nome_tabela, operacao, valor_novo)
-  VALUES (log_seq.NEXTVAL, 'Produto', 'INSERT', :NEW.nome);
-END;
 
 -- b.	Trigger para UPDATE na tabela Produto:
 
 CREATE OR REPLACE TRIGGER log_update_produto
-AFTER UPDATE ON Produto
+AFTER UPDATE ON Produtos
 FOR EACH ROW
 BEGIN
   INSERT INTO log (id, nome_tabela, operacao, valor_atual, valor_novo)
-  VALUES (log_seq.NEXTVAL, 'Produto', 'UPDATE', OLD.nome, NEW.nome);
+  VALUES (log_seq.NEXTVAL, 'Produtos', 'UPDATE', :OLD.nome, :NEW.nome);
 END;
 
 -- c. Trigger para DELETE na tabela Produto:
 
 CREATE OR REPLACE TRIGGER log_delete_produto
-AFTER DELETE ON Produto
+AFTER DELETE ON Produtos
 FOR EACH ROW
 BEGIN
   INSERT INTO log (id, nome_tabela, operacao, valor_atual)
-  VALUES (log_seq.NEXTVAL, 'Produto', 'DELETE', OLD.nome);
+  VALUES (log_seq.NEXTVAL, 'Produtos', 'DELETE', :OLD.nome);
 END;
 
 -- 4) Testar as Triggers
 -- a) Inserindo um Produto
 
-INSERT INTO Produto (id, nome) VALUES (1, 'Livro Aventura');
+INSERT INTO Produtos (id_produtos, nome, descricao, preco, estoque) 
+                VALUES (2, 'Livro Aventura', 'Um livro de romance e aventura', 150, 50);
 
 
 -- b) Atualizando o Produto
-UPDATE Produto SET nome = 'Livro Aventura 2' WHERE id = 1;
+UPDATE Produtos SET nome = 'Livro Aventura 2' WHERE id = 1;
 
 -- c) Deletando o Produto
 
-DELETE FROM Produto WHERE id = 1;
+DELETE FROM Produtos WHERE id_produtos = 2;
 
 --  5) Consultar a Tabela de Logs
 SELECT * FROM log;
+
 
 
 
